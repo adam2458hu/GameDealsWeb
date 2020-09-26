40a3b47209c8673b2eeb379e512800d4003defff
Ádám Sipos <adam2458hu@gmail.com>
2020-09-26 21:36:12 +0200

    GoogleAds hozzáadása

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -68,4 +70,17 @@ mongoose.connection.on('connected',()=>{
     setInterval(usersRoutes.deleteUnverifiedUsers,1800000);
 		//usersRoutes.sendWaitlistEmails();
 	});
+  /*require("greenlock-express")
+    .init({
+        packageRoot: __dirname,
+ 
+        // contact for security and critical bug notices
+        configDir: "./greenlock.d",
+ 
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);*/
 });
