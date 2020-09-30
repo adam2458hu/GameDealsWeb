cb5d2e933090d0ea3df09bf064bc30e50b304ea5
Ádám Sipos <adam2458hu@gmail.com>
2020-09-30 20:00:28 +0200

    Számot ellenőrző validátor

---
+++ src/app/shared/has-number-validator.directive.ts
@@ -0,0 +1,23 @@
+import { Validator, NG_VALIDATORS, AbstractControl } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+
+@Directive({
+	selector: '[appHasNumberValidator]',
+	providers: [{
+		provide: NG_VALIDATORS,
+		useExisting: HasNumberValidatorDirective,
+		multi: true
+	}]
+})
+
+export class HasNumberValidatorDirective implements Validator{
+	@Input() appHasNumberValidator: string;
+	validate(control: AbstractControl): { [key:string]:any } | null {
+		//const controlToCompare = control.parent.get(this.appEqualPasswordValidator);
+		if (!/\d/.test(control.value)) {
+			return { 'needsNumber' : true };
+		}
+
+		return null;
+	}
+}
