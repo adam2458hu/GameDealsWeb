db8d19efec82da11961b3739f2895868e10937fe
Ádám Sipos <adam2458hu@gmail.com>
2020-09-30 20:00:02 +0200

    Kis betűt ellenőrző validátor

---
+++ src/app/shared/has-lowercase-validator.directive.ts
@@ -0,0 +1,23 @@
+import { Validator, NG_VALIDATORS, AbstractControl } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+
+@Directive({
+	selector: '[appHasLowercaseValidator]',
+	providers: [{
+		provide: NG_VALIDATORS,
+		useExisting: HasLowercaseValidatorDirective,
+		multi: true
+	}]
+})
+
+export class HasLowercaseValidatorDirective implements Validator{
+	@Input() appHasLowercaseValidator: string;
+	validate(control: AbstractControl): { [key:string]:any } | null {
+		//const controlToCompare = control.parent.get(this.appEqualPasswordValidator);
+		if (control.value.toUpperCase()==control.value) {
+			return { 'needsLowercase' : true };
+		}
+
+		return null;
+	}
+}
